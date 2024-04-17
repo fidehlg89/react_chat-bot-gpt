6d2438cbd6dc84356fe3d4d73cd1fcd713431f09
Fidel Ernesto <fidehlg89@gmail.com>
2024-04-16 23:33:18 -0400

    * feat(chatBot): updated chat bot name

--- src/components/widgets/chatBot/utils/Constants.jsx
+++ src/components/widgets/chatBot/utils/Constants.jsx
@@ -1,5 +1,5 @@
 class Constants {
-  static CHAT_BOT_NAME = "Chat bot name";
+  static CHAT_BOT_NAME = "A custom chat bot";
   static STARTER_CHAT_MSG = "Start message";
   static SERVICES_WIDGET_NAME = "Services widget name example";
   static HELLO_MSG = "Hello world";
